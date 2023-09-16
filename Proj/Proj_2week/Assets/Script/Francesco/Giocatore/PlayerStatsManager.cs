67563f0b5c0b2d124eb95ed13a8b36d5675d85a4
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2023-09-16 17:50:26 +0200

    Un po' di cambiamenti
    Spostati gli script e sistemati il salto e PowerUp (Scriptable Obj)

--- Proj/Proj_2week/Assets/Script/Francesco/Giocatore/PlayerStatsManager.cs
+++ Proj/Proj_2week/Assets/Script/Francesco/Giocatore/PlayerStatsManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerStatsManager : MonoBehaviour
+public class PlayerStatsManager : MonoBehaviour, IDamageable
 {
     [SerializeField] PlayerStatsSO_Script stats_SO;
     
@@ -15,12 +15,23 @@ public class PlayerStatsManager : MonoBehaviour
         //Quando devo utilizzare il powerup
         if (Input.GetKeyDown(KeyCode.Mouse1))
         {
-            //TODO: usa il powerup
+            PowerUp used_PU = stats_SO.UsePowerUp();
+
+            //TODO: sistema
+            //
+            //used_PU.Activate();
+        }
+
+
+        //Quando muore
+        if (isDead)
+        {
+            ;
         }
     }
 
 
-    public void DamagePlayer()
+    public void Die()
     {
         isDead = true;
     }
