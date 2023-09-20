7f3e667f1c28c8fb341b90fc2ba120cb13dcc6fa
Green-Hat-fd <108188257+Green-Hat-fd@users.noreply.github.com>
2023-09-20 17:58:17 +0200

    Audio e minimi Y
    Aggiunti audio (suoni e musica), sistemate le stats del giocatore (min Y), aggiunto un min Y per la telecamera, rinominate scene (Francesco --> Main, SampleScene --> Francesco)

--- Proj/Proj_2week/Assets/Script/Francesco/Giocatore/PlayerStatsManager.cs
+++ Proj/Proj_2week/Assets/Script/Francesco/Giocatore/PlayerStatsManager.cs
@@ -8,8 +8,16 @@ public class PlayerStatsManager : MonoBehaviour, IDamageable
     public bool isDamageable = true;
     bool isDead;
 
+    [Space(20)]
+    [Range(-100, 0)]
+    [SerializeField] float yMinDeath = -10;
+
     [Header("—— Feedback ——")]
     [SerializeField] AudioSource deathSfx;
+    [SerializeField] Canvas deathCanvas;
+
+    [Header("—— DEBUG ——")]
+    [SerializeField] float deathZoneSize = 15;
 
 
 
@@ -26,10 +34,10 @@ public class PlayerStatsManager : MonoBehaviour, IDamageable
         }
 
 
-        //Quando muore
-        if (isDead)
+        //Muore quando supera il limite minimo sulla Y
+        if (transform.position.y <= yMinDeath)
         {
-            ;
+            Die();
         }
     }
 
@@ -41,8 +49,32 @@ public class PlayerStatsManager : MonoBehaviour, IDamageable
 
         #region Feedback
 
+        //Mostra la canvas di Game Over
+        deathCanvas.gameObject.SetActive(true);
+
+        //Audio
         deathSfx.Play();
 
         #endregion
     }
+
+
+
+    #region EXTRA - Gizmos
+
+    private void OnDrawGizmosSelected()
+    {
+        Vector3 deathOffset = Vector3.up * yMinDeath,
+                cubeOffset = Vector3.up * (deathZoneSize / 2),
+                pos_yDeath = Vector3.zero + deathOffset - cubeOffset;
+        pos_yDeath.x = transform.position.x;
+
+        //Disegna un rettangolo dove il giocatore muore
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireCube(pos_yDeath, new Vector3(deathZoneSize * 2,
+                                                    deathZoneSize ,
+                                                    deathZoneSize * 2));
+    }
+
+    #endregion
 }
