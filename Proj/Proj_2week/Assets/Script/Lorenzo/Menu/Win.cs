9c8c6927e6173ba31be647f17c3a0df932c4a9e2
lorenzomusumeci <124389907+lorenzomusumeci@users.noreply.github.com>
2023-09-24 19:25:42 +0200

    Level adjust

---
+++ Proj/Proj_2week/Assets/Script/Lorenzo/Menu/Win.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Win : MonoBehaviour
+{
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void ReturnToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
